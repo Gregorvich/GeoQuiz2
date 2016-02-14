f796b991696423398f16cbe450c90dc9bb5b8249
Adam <agregor@gustavus.edu>
2016-02-14 12:22:18 -0600

    Test

--- app/src/main/java/com/bignerdranch/android/geoquiz2/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz2/QuizActivity.java
@@ -49,12 +49,6 @@ public class QuizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
-        mQuestionTextView.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                nextQuestion();
-            }
-        });
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
@@ -77,16 +71,12 @@ public class QuizActivity extends AppCompatActivity {
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                nextQuestion();
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
             }
         });
 
         updateQuestion();
 
-    }
-
-    private void nextQuestion() {
-        mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-        updateQuestion();
     }
 }
